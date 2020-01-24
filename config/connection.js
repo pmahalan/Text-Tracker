b9d8d1064a43ebbbbfff6aa12582b07298e5585b
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-01-24 13:32:37 -0500

    changed the connection info AGAIN to reflect our use of sequelize

--- config/connection.js
+++ config/connection.js
@@ -2,34 +2,32 @@
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
 
-var mysql2 = require("mysql2");
-var connection;
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize("sequelize_people_db", "root", "root", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+module.exports = sequelize;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql2.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql2.createConnection({
-    host: "vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "	kbkd2frusi0yuu95",
-    password: "ywaisq4lta2uh714",
-    database: "	gcjl719enzijbntb"
-  });
-};
+// var mysql2 = require("mysql2");
+// var connection;
 
-connection.connect();
-module.exports = connection;
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql2.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql2.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: null,
+//     database: "people_db"
+//   });
+// };
 
-// OLD CONNECTION INFO (pre-jaws db!)
-
-// var Sequelize = require("sequelize");
-// var sequelize = new Sequelize("sequelize_people_db", "root", "root", {
-//   host: "localhost",
-//   port: 3306,
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   }
-// });
-// module.exports = sequelize;
+// connection.connect();
+// module.exports = connection;
